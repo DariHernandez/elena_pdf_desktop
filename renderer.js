4e4ab0d0231f1cf812288bc19d43b101668d2b86
dari <hernandezdarifrancisco@gmail.com>
2021-01-07 20:35:02 -0600

    Drop files and get path

---
+++ renderer.js
@@ -0,0 +1,27 @@
+function drop_files () {
+    var holder = document.getElementById('drag-file');
+
+    holder.ondragover = () => {
+        return false;
+    };
+
+    holder.ondragleave = () => {
+        return false;
+    };
+
+    holder.ondragend = () => {
+        return false;
+    };
+
+    holder.ondrop = (e) => {
+        e.preventDefault();
+
+        for (let f of e.dataTransfer.files) {
+            console.log(f.path)
+        }
+        
+        return false;
+    };
+};
+
+drop_files()
--- src/js/sections.js
+++ src/js/sections.js
@@ -1,21 +1,101 @@
-let dropContainer = document.querySelector("#dropContainer")
+// Detect drop evenets
 
-dropContainer.ondragover = dropContainer.ondragenter = function(evt) {
-    evt.preventDefault()
+var holder = document.querySelector(".drop_area")  
+text_drop_area = holder.innerHTML
+var files = []
+
+function drop_files () {
+
+    holder.ondraenter = () => {
+        return false
+    };
+
+    holder.ondragover = () => {
+        holder.innerHTML = "DROP!"
+        holder.classList.add ("active")
+        return false
+    };
+
+    holder.ondragleave = () => {
+        holder.classList.remove ("active")
+        holder.innerHTML = text_drop_area
+        return false;
+    };
+
+    holder.ondrop = (e) => {
+
+        // Add to list path of each file
+        for (let f of e.dataTransfer.files) {
+            files.push(f.path)
+        }
+
+        e.preventDefault();
+
+        // Reestart section
+        holder.classList.remove ("active")
+        holder.innerHTML = ""
+
+        // Add file to html
+        add_file ()
+
+        return false;
+    };
+
 };
 
-let files = []
+function add_file () {
+    // Add file to html grid files
+    holder.classList.add ("files")
 
-dropContainer.ondrop = function(evt) {
+    // Generate childs
+    text_drop_area = ""
+    text_drop_area += '<div class="grid_files">'
 
-    // If you want to use some of the dropped files
-    const dT = new DataTransfer();
-    dT.items.add(evt.dataTransfer.files[0]);
+    for (file in files) {
+        text_drop_area += '<div class="file_document">'
+    }
+                
+    text_drop_area += '</div>'
 
-    // save files
-    files.push (dT)
+    // Add childs
+    holder.innerHTML = text_drop_area
 
-    evt.preventDefault()
-
-    console.log(files)
 }
+
+//  Call funtion 
+drop_files ()
+
+
+// Detect drop evenets
+
+// let files = []
+
+// var holder = document.querySelector(".drop_area")
+// text_drop_area = holder.innerHTML
+
+// holder.addEventListener("dragover", function () {
+//     holder.classList.add ("active")
+//     holder.innerHTML = "DROP!"
+//     return false
+// })
+
+
+// holder.addEventListener("dragleave", function () {
+//     holder.classList.remove ("active")
+//     holder.innerHTML = text_drop_area
+//     return false
+// })
+
+// holder.addEventListener("ondrop", function (e) {
+//     ondrop (e)
+// })
+
+// function ondrop (e) {
+//     e.preventDefault();
+
+//     for (let f of e.dataTransfer.files) {
+//         console.log(f.path)
+//     }
+    
+//     return false;
+// }
