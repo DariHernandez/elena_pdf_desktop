bb5d63faf333a48c722439cfe4f3216c4fa5e49e
dari <hernandezdarifrancisco@gmail.com>
2021-01-11 03:10:44 -0600

    Bottons section funtionality and disabled
    Block the funtionality of the buttons secctions where there aren't the min number of files in list
    Add styles to disabled buttons

--- src/js/sections.js
+++ src/js/sections.js
@@ -1,15 +1,17 @@
-// Detect cick events
+// Control variables for buttons status
+var name_date_active_button = false
+var delete_active_button = false
 
+// Get buttons and add events
+order_name_buttons = document.querySelectorAll (".order_name")
+order_date_buttons = document.querySelectorAll (".order_date")
+delete_buttons = document.querySelectorAll (".delete")
 
-order_name_button = document.querySelector (".order_name")
-order_date_button = document.querySelector (".order_date")
-delete_button = document.querySelector (".delete")
-theme_button = document.querySelector (".theme")
-
-order_name_button.addEventListener ('click', function(){short_files("name")})
-order_date_button.addEventListener ('click', function(){short_files("date")})
-delete_button.addEventListener ('click', delete_files)
-
+for (let i = 0; i < order_name_buttons.length; i++) {
+    order_name_buttons[i].addEventListener ('click', function(){short_files("name")})
+    order_date_buttons[i].addEventListener ('click', function(){short_files("date")})
+    delete_buttons[i].addEventListener ('click', delete_files)
+}
 
 // Detect drop evenets
 var holder = document.querySelector(".drop_area")  
@@ -83,6 +85,9 @@ function drop_files () {
 };
 
 function update_files () {
+    // Update buttons
+    update_buttons_status ()
+
     // Add file to html grid files
     holder.classList.add ("files")
 
@@ -118,73 +123,113 @@ function update_files () {
 reverse_short = false
 function short_files (order_by) {
     
-    let len = files.length;
-    let swapped;
-    do {
-        swapped = false;
-        for (let i = 0; i < len - 1; i++) {
+    // Verify buttons status
+    if (name_date_active_button == true ) {
 
-            condition_sort = false
-            if (order_by == "name") {
-                if (files[i].name > files[i + 1].name) {
-                    condition_sort = true
-                }
-            } else if (order_by == "date") {
-                if (files[i].lastModified > files[i + 1].lastModified) {
-                    condition_sort = true
-                }        
-            }
-
-            if (condition_sort == true) {
-                let tmp = files[i];
-                files[i] = files[i + 1];
-                files[i + 1] = tmp;
-                swapped = true;
-            }
-        }
-    } while (swapped);
-
-    
-    // Reverse files
-    if (reverse_short == true) {
-
-        let files_reverse = []
         let len = files.length;
+        let swapped;
+        do {
+            swapped = false;
+            for (let i = 0; i < len - 1; i++) {
 
-        for (let i = len - 1; i >= 0; i--) {
-            files_reverse.push (files[i])
-        }
+                condition_sort = false
+                if (order_by == "name") {
+                    if (files[i].name > files[i + 1].name) {
+                        condition_sort = true
+                    }
+                } else if (order_by == "date") {
+                    if (files[i].lastModified > files[i + 1].lastModified) {
+                        condition_sort = true
+                    }        
+                }
 
-        files = files_reverse
+                if (condition_sort == true) {
+                    let tmp = files[i];
+                    files[i] = files[i + 1];
+                    files[i + 1] = tmp;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
 
-        reverse_short = false
+        
+        // Reverse files
+        if (reverse_short == true) {
 
-        alert (`Files shorted by ${order_by} (reverse)`)
+            let files_reverse = []
+            let len = files.length;
 
-    } else {
-        reverse_short = true
+            for (let i = len - 1; i >= 0; i--) {
+                files_reverse.push (files[i])
+            }
 
-        alert (`Files shorted by ${order_by}`)
-    }    
+            files = files_reverse
 
-    // Update
-    update_files ()
+            reverse_short = false
+
+            alert (`Files shorted by ${order_by} (reverse)`)
+
+        } else {
+            reverse_short = true
+
+            alert (`Files shorted by ${order_by}`)
+        }    
+
+        // Update
+        update_files ()
+    }
 }
 
 function delete_files () {
-    if (confirm("Do you want to delete all files?")) {
-        files = []
-        update_files()
-        restart_drop_area ()
-    } 
+    // Verify button status
+    if (delete_active_button == true) {
+        if (confirm("Do you want to delete all files?")) {
+            files = []
+            update_files()
+            restart_drop_area ()
+        } 
+    }
 }
 
-function update_buttons () {
+function update_buttons_status () {
+    // Verify files to update status
+    if (files.length > 0) {
+        delete_active_button = true
+        
+        if (files.length > 1) {
+            name_date_active_button = true
+        } else {
+            name_date_active_button = false
+        }
 
+    } else {
+        delete_active_button = false
+        name_date_active_button = false
+    }
+
+    // Update class list
+    for (let i = 0; i < order_name_buttons.length; i++) {
+        if (name_date_active_button == true) {
+            order_name_buttons[i].classList.remove ("disabled")
+            order_date_buttons[i].classList.remove ("disabled")
+        } else {
+            order_name_buttons[i].classList.add ("disabled")
+            order_date_buttons[i].classList.add ("disabled")
+        }
+
+        if (delete_active_button == true) {
+            delete_buttons[i].classList.remove ("disabled")
+        } else {
+            delete_buttons[i].classList.add ("disabled")
+        }
+    }
 }
 
 
 //  Call funtion 
 drop_files ()
+
+// Initial button status
+update_buttons_status()
 
 
